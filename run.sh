f18e8519eb8dc8075d42e6068fb3a66465144944
norafathima <norafathima@hotmail.com>
2025-07-04 19:37:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Update and install dependencies
-apt update
-apt install -y python3 python3-pip g++
+sudo apt update
+sudo apt install -y python3 python3-pip g++
 
 # Install Python dependencies
 pip3 install pyTelegramBotAPI
@@ -12,5 +12,6 @@ g++ soul.cpp -o game -pthread
 chmod +x game
 
 # Start the bot
-pip install python-telegram-bot==13.15
+pip install --upgrade pip
+pip install python-telegram-bot==13.15 urllib3 six
 python3 bot.py
